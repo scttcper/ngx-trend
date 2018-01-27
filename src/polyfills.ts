aec0216cf8b9023d7225b9efe5df9f821315587f
Scott Cooper <scttcper@gmail.com>
2018-01-27 00:20:55 -0800

    update dep

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -37,6 +37,7 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
+
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
@@ -48,21 +49,13 @@ import 'core-js/es7/reflect';
  **/
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
+
 /***************************************************************************************************
- * Zone JS is required by Angular itself.
+ * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-// import 'intl';  // Run `npm install --save intl`.
-/**
- * Need to import at least one locale-data with intl.
- */
-// import 'intl/locale-data/jsonp/en';
