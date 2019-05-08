a1881fcf8930a85b99ce1997c6ea383d8e32eb58
Scott Cooper <scttcper@gmail.com>
2019-05-08 00:13:29 -0700

    build: use circle ci, upgrade to angular 8

--- karma.conf.js
+++ karma.conf.js
@@ -10,22 +10,19 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
-    angularCli: {
-      environment: 'dev',
-    },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'kjhtml'],
     port: 9876,
-    browserDisconnectTimeout: 10000,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
@@ -37,5 +34,6 @@ module.exports = function(config) {
       },
     },
     singleRun: false,
+    restartOnFileChange: true,
   });
 };
